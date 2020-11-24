4fb4407a7c9616d1b90977b03c44b29f1fae1c68
harutiro <harutiro2027@outlook.jp>
2020-11-25 02:17:49 +0900

    11/25

--- zikken-program.cpp
+++ zikken-program.cpp
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <windows.h>
 
-int main(void){
-    
-    printf("hello world\n");
-    system("pause");
-    return 0;
+void kusa (int);
+void kaigyou (void);
+void sasa (void);
 
+int main (void){
+   kusa(5);
+   system("pause");
+   return 0;
+}
+
+void kusa (int num){
+   int i;
+   for(i=num;i>0;i--){
+      printf("‘");
+   }
+   kaigyou();
+   return ;
+}
+
+void kaigyou(void){
+   printf("\n");
+   sasa();
+   return ;
+}
+
+void sasa(void){
+   printf("ù");
+   sasa();
+   return ;
 }
