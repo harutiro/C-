e1a525a26397b486e00208dcb8e7af962659f250
harutiro <harutiro2027@outlook.jp>
2020-11-29 17:18:06 +0900

    11/29

--- kansuu2.c
+++ kansuu2.c
@@ -1,5 +1,19 @@
-#include <stdio.h>
-int main(void){
-    
+#include<stdio.h>
+#include<windows.h>
+int tri(int,int);
 
-} 
+int main(void)
+{
+	int side,high,square;
+	
+	scanf("%d,%d",&side,&high);
+	printf("%d\n",tri(side,high));
+	
+    system("pause");
+	return 0;
+}
+
+int tri(int side,int high)
+{
+	return side * high / 2;
+}
